ca1ec0ed5272687a1fa44460ecb184f6e822e1ec
Tinnmann <tinmadzin@gmail.com>
2019-09-17 04:33:06 +0200

    updating home view and fixing the detail view

--- topFive/views.py
+++ topFive/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render
 from topFive.models import TopFive
 
 # Create your views here.
-def novelSeriesHome(request):
-    return render(request, 'novelSeriesHome.html', {})
-
 def novelSeriesIndex(request):
-    lotrs = TopFive.objects.get(seriesTitle="The Lord of The Rings")
-    sots = TopFive.objects.get(seriesTitle="The Sword of Truth")
-    hps = TopFive.objects.get(seriesTitle="Harry Potter")
-    tdfs = TopFive.objects.get(seriesTitle="The Dreden Files")
-    otherlands = TopFive.objects.get(seriesTitle="Otherland")
+    lotrs = TopFive.objects.filter(seriesTitle="The Lord of The Rings")
+    sots = TopFive.objects.filter(seriesTitle="The Sword of Truth")
+    hps = TopFive.objects.filter(seriesTitle="Harry Potter")
+    tdfs = TopFive.objects.filter(seriesTitle="The Dresden Files")
+    otherlands = TopFive.objects.filter(seriesTitle="Otherland")
     context = {
         'lotrs': lotrs,
         'sots': sots,
@@ -21,16 +18,8 @@ def novelSeriesIndex(request):
     return render(request, 'novelSeriesIndex.html', context)
 
 def novelSeriesDetail(request, pk):
-    lotr = TopFive.objects.get(seriesTitle="The Lord of The Rings", pk=pk)
-    sot = TopFive.objects.get(seriesTitle="The Sword of Truth", pk=pk)
-    hp = TopFive.objects.get(seriesTitle="Harry Potter", pk=pk)
-    tdf = TopFive.objects.get(seriesTitle="Otherland", pk=pk)
-    otherland = TopFive.objects.get(seriesTitle="Otherland", pk=pk)
+    novel = TopFive.objects.get(pk=pk)
     context = {
-        'lotr': lotr,
-        'sot': sot,
-        'hp': hp,
-        'tdf': tdf,
-        'otherland': otherland
+        'novel': novel
     }
     return render(request, 'novelSeriesDetail.html', context)
